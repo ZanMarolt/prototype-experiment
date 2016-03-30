958f9e1b05668d87304b479b4be8e28b53bcf761
Zan Marolt <marolt.zan@hotmail.com>
2016-03-30 11:19:47 +0200

    added mouseleave event, drop-shadow to the circles

--- js/app.js
+++ js/app.js
@@ -1,60 +1,110 @@
-/**
- * Created by zanmarolt on 3/29/16.
- */
 var app = {
-
     circles:[],
     numCircles:4,
-    circleSize:100,
+    circleSize:50,
     padding:5,
     container:null,
     init:function(){
 
         app.renderContainer();
         app.render();
-        setTimeout(function(){
-
-            for(var i=0;i<app.circles.length;i++){
-                var circle = app.circles[i];
-                //circle.removeEvents();
-            }
-
-        }, 4000)
+        app.addClickEvents();
 
     },
     renderContainer:function(){
 
-        app.container = $('<div>', {class:'circle-container'});
+        app.container = $('<div>',{class:'circle-container'});
         $('body').append(app.container);
 
     },
     render:function(){
 
-        var widthSum = app.numCircles*app.circleSize+(app.numCircles-1)*app.padding;
+        var widthSum    = app.numCircles*app.circleSize+(app.numCircles-1)*app.padding;
         var topOffset   = $(window).height()/2-app.circleSize/2;
 
         while(app.numCircles--){
 
-            var circle = new Circle(app.circleSize,app.circleSize, 'body', app.container);
+            var circle = new Circle(
+                app.numCircles,
+                app.circleSize,
+                app.circleSize,
+                app.container,
+                'circle-template-'+app.numCircles);
+
             circle.setPosition({
-               y:topOffset,
-                x:(app.circleSize+app.padding)*app.numCircles
+                x : (app.circleSize+app.padding)*app.numCircles,
+                y : 0
             });
             app.circles.push(circle);
 
         }
 
         app.container.css({
-            left            : '50%',
-            marginLeft      : -widthSum/2
+            left        : '50%',
+            y           : topOffset,
+            marginLeft  : -widthSum/2
         });
 
     },
     addClickEvents:function(){
 
+        for(var i=0;i<app.circles.length;i++){
 
+            var circle = app.circles[i];
+            circle.circleElement.on('click', function(){
+
+                var _circle = this;
+
+                app.fadeCirclesOut(this.id);
+
+                setTimeout(function(){
+
+                    app.scaleUpContainer();
+                    _circle.removeEvents();
+                    _circle.scaleUp();
+
+                },1000);
+
+
+            }.bind(circle));
+
+        }
+
+    },
+    scaleUpContainer:function(){
+
+        app.container.transition({
+            width:$(window).width(),
+            height:$(window).height(),
+            top:0,
+            y:0,
+            left:0,
+            margin:0
+        });
+
+    },
+    resetContainer:function(){
+
+        app.container.css({
+            left        : '50%',
+            y           : topOffset,
+            marginLeft  : -widthSum/2
+        });
+
+    },
+    fadeCirclesOut:function(id){
+
+        for(var i=0;i<app.circles.length;i++){
+
+            var circle = app.circles[i];
+            if(circle.id !== id){
+                circle.fadeOut();
+            }
+
+        }
 
     }
+
 };
 
 app.init();
--- js/circle.js
+++ js/circle.js
@@ -1,17 +1,16 @@
-/**
- * Created by zanmarolt on 3/29/16.
- */
-var Circle = function(width, height, container){
+var Circle = function(id, width, height, container, templateName){
 
-    this.width = width;
+    // constructor function
+    // function executes when new instance of Circle is created
+    this.id     = id;
+    this.templateName = templateName;
+    this.width  = width;
     this.height = height;
 
     this.render(container);
     this.addEvents();
-};
 
-// prototypes are basically a better alternative to attaching a function to
-// this.function(); except it is on the top level
+};
 
 Circle.prototype.setPosition = function(properties){
 
@@ -19,16 +18,52 @@ Circle.prototype.setPosition = function(properties){
 
 };
 
-Circle.prototype.render = function(container) {
+Circle.prototype.fadeOut = function(){
 
-    this.circleElement = $('<div>', {class:'circle'});
+    this.circleElement.transition({opacity:0, scale:0.2});
+
+};
+
+Circle.reset = function(){
+
+
+
+};
+
+Circle.prototype.scaleUp = function(){
+
+    this.circleElement.transition({
+        width:$(window).width(),
+        height:'100vh',
+        borderRadius:0,
+        scale:1,
+        y:0,
+        x:0,
+        top:0,
+        left:0
+    });
+
+    this.circleElement.find('.circle-content').show();
+
+};
+
+Circle.prototype.render = function(container){
+
+    var template = $('#'+this.templateName).html();
+
+    this.circleElement = $('<div>',{ class:'circle'});
+    this.circleElement.append(template);
+
+    this.circleElement.find('.circle-content').hide();
+
     this.circleElement.css({
         width:this.width,
         height:this.height,
-        x:Math.random()*$(window).height(),
-        y:Math.random()*$(window).width()
+        x:Math.random()*$(window).width(),
+        y:Math.random()*$(window).height()
     });
     $(container).append(this.circleElement);
+
 };
 
 Circle.prototype.removeEvents = function(){
@@ -44,6 +79,19 @@ Circle.prototype.addEvents = function(){
     this.circleElement.on('mouseenter', function(){
 
         $(self.circleElement).transition({scale:1.5});
+        $(self.circleElement).css({
+           zIndex:999
+        });
 
-    })
+    });
+
+    this.circleElement.on('mouseleave', function(){
+
+        $(self.circleElement).transition({scale:1});
+        $(self.circleElement).css({
+            zIndex:0
+        });
+
+    });
+
 };
